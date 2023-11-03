2bdf929ccf3c6183fe077890cc2ff18ae1b7ac03
Érick <erick.solon.prog@gmail.com>
2023-11-03 01:38:15 -0300

    tests

---
+++ core/test.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from .models import InformacoesSensiveis, Pessoas
+
+class PessoasTestCase(TestCase):
+    
+    
+    def test_model_pessoas_existe(self):
+        dados = Pessoas.objects.count()
+        self.assertEqual(dados, 0)
+        
+    def test_model_informacoesSensiveis_existe(self):
+        informacoes = InformacoesSensiveis.objects.count()
+        self.assertIsNotNone(informacoes, 0)
